cd0a6108dfc5756f9bf6f9e18d2b1b7b6720c774
sh <seokjin.han@standigm.com>
2022-01-01 14:16:49 +0900

    FIx styles and add plot support (#3)
    * Update CSS (colorscheme, fontsize)
    * Hide table of content in touch mode
    * Add Vega-lite plot support

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
                 { from: 'node_modules/katex/dist/katex.min.css', to: '../static/css/' },
                 { from: 'node_modules/katex/dist/katex.min.js', to: '../static/js/' },
                 { from: 'node_modules/katex/dist/contrib/auto-render.min.js', to: '../static/js/' },
+                { from: 'node_modules/katex/dist/katex.min.css', to: '../static/css/' },
+                { from: 'node_modules/vega/build/vega.min.js', to: '../static/js/' },
+                { from: 'node_modules/vega/build/vega-schema.json', to: '../static/schema/' },
+                { from: 'node_modules/vega-lite/build/vega-lite.min.js', to: '../static/js/' },
+                { from: 'node_modules/vega-lite/build/vega-lite-schema.json', to: '../static/schema/' },
+                { from: 'node_modules/vega-embed/build/vega-embed.min.js', to: '../static/js/' },
+                { from: 'node_modules/vega-themes/build/vega-themes.min.js', to: '../static/js/' },
             ]
         })
     ],
